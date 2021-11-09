816fa36a6da40755ac4ae2acb9959d18170fd99a
RobinDev <contact@piedweb.com>
2021-11-09 20:52:39 +0000

    remove dead code

--- src/Scanner/PageScannerService.php
+++ src/Scanner/PageScannerService.php
@@ -73,7 +73,7 @@ final class PageScannerService
             return '';
         }
 
-        return $response->getContent();
+        return (string) $response->getContent();
     }
 
     private function addErrors(PageInterface $page, array $messages): void
