acd1025024fd19b379c559b415082e2c0e99ca76
RobinDev <contact@piedweb.com>
2021-11-11 11:09:56 +0000

    ...

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -18,8 +18,8 @@ class Configuration implements ConfigurationInterface
                     //->variableNode('app_fallback_properties')->defaultValue(self::DEFAULT_APP_FALLBACK)->cannotBeEmpty()->end()
                     ->scalarNode('min_interval_between_scan')
                         ->defaultValue('PT5M')->cannotBeEmpty()
-                    ->end()
-                ->end()
+                    //->end()
+                //->end()
         ;
 
         return $treeBuilder;
